e21b667974606332b815da2b23e052550470f527
Yelyzaveta Drozd <elizavetadrozd266@knu.ua>
2023-02-13 21:01:39 +0200

    Edited some logic.

--- main.py
+++ main.py
@@ -2,13 +2,7 @@ from random import randint
 
 
 def Game():
-    print("Hi! Let`s start a game!\nChoose the number:")
-    try:
-        choice = int(input("1 - start the game\n0 - read the rules\n"))
-    except:
-        print("\033[31m{}".format("Wrong input, try again"))
 
-    if choice == 1:
         print("Enter your name: ")
         player_name = input()
         print("%s, choose a gesture:" % player_name)
@@ -16,7 +10,7 @@ def Game():
             number_of_gesture = int(input("1 - Rock\n2 - Paper\n3 - Scissors\n4 - Lizard\n5 - Spock\n"))
             if number_of_gesture in (1, 2, 3, 4, 5):
                 bot_gesture = randint(1, 5)
-                # result(number_of_gesture, bot_gesture, player_name)
+                result(number_of_gesture, bot_gesture, player_name)
             else:
                 print("\033[31m{}".format("You choose a wrong number, try again"))
         except:
