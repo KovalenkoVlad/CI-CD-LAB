1770f580f36fdbfb472a1fcbf39ab0f420c6efaf
Yelyzaveta Drozd <elizavetadrozd266@knu.ua>
2023-02-13 20:47:45 +0200

    Added game flow.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+from random import randint
 
 
 def Game():
@@ -8,6 +8,19 @@ def Game():
     except:
         print("\033[31m{}".format("Wrong input, try again"))
 
+    if choice == 1:
+        print("Enter your name: ")
+        player_name = input()
+        print("%s, choose a gesture:" % player_name)
+        try:
+            number_of_gesture = int(input("1 - Rock\n2 - Paper\n3 - Scissors\n4 - Lizard\n5 - Spock\n"))
+            if number_of_gesture in (1, 2, 3, 4, 5):
+                bot_gesture = randint(1, 5)
+                result(number_of_gesture, bot_gesture, player_name)
+            else:
+                print("\033[31m{}".format("You choose a wrong number, try again"))
+        except:
+            print("\033[31m{}".format("Wrong input, try again"))
 
 
 Game()
