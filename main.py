b39ad7b2cb9913109f876689fc5f375d76bfe875
KovalenkoVlad <kovalenkovlad420@knu.ua>
2023-02-13 21:23:04 +0200

    Fixed bag

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def Game():
         number_of_gesture = int(input("1 - Rock\n2 - Paper\n3 - Scissors\n4 - Lizard\n5 - Spock\n"))
         if number_of_gesture in (1, 2, 3, 4, 5):
             bot_gesture = randint(1, 5)
-            result(number_of_gesture, bot_gesture, player_name)
+            print(result(number_of_gesture, bot_gesture, player_name))
         else:
             print("\033[31m{}".format("You choose a wrong number, try again"))
     except:
