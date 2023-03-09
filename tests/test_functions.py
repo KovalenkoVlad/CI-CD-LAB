7d856563ec14b575b5ad5c53d1a2959a2f14c5e9
KovalenkoVlad <kovalenkovlad420@knu.ua>
2023-03-09 11:50:55 +0200

    Add test for win function

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,4 +1,7 @@
-from main import lose
+from main import lose,win
 
 def test_lose_message():
     assert lose() == "You lost. Unfortunately! Try again)"
+
+def test_win_message():
+    assert win() == "You win!Congratulations!"
