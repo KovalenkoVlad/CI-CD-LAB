6e414d49e2c9dfde7fa7df38fc1bba8a03e75548
KovalenkoVlad <kovalenkovlad420@knu.ua>
2023-03-09 18:08:53 +0200

    Add test for tie function

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,10 @@
-from main import lose,win
+from main import lose,win,tie
 
 def test_lose_message():
     assert lose() == "You lost. Unfortunately! Try again)"
 
 def test_win_message():
     assert win() == "You win!Congratulations!"
+
+def test_tie_message():
+    assert tie() == "Tie"
